944148a8ebe5816fc8948da0a2ffa0cf94e49cb4
Sourav Sardar <soourav8@gmail.com>
2023-04-12 14:51:04 +0530

    get local storage data

--- src/components/Applied/Applied.jsx
+++ src/components/Applied/Applied.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Applied = () => {
+    const [items , setItems] = useState([]);
     const data = useLoaderData()
-    console.log(data)
+    useEffect(()=>{
+        const storedId = getShoppingCart()
+        const savedData = [];
+        for(const id in storedId){
+            const appliedData = data.find(data => data._id ===id );
+            savedData.push(appliedData);
+          
+
+        }
+        setItems(savedData)
+
+        
+        
+       
+        
+    },[items])
+    
+    
+    
+    // const job = data.find(data => data._id === )
+    
     return (
         <div>
-           
+           <p>{items.length}</p>
         </div>
     );
 };
